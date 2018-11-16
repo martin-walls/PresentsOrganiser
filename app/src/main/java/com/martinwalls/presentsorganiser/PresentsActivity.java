1d4bc6329bde935691a628c209760d5027de0206
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2018-11-16 18:16:05 +0000

    onCreate of PresentsActivity

--- app/src/main/java/com/martinwalls/presentsorganiser/PresentsActivity.java
+++ app/src/main/java/com/martinwalls/presentsorganiser/PresentsActivity.java
@@ -1,30 +1,60 @@
 package com.martinwalls.presentsorganiser;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class PresentsActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class PresentsActivity extends AppCompatActivity
+        implements PresentsAdapter.PresentsAdapterListener {
+
+    public static String PRESENTS_TO_SHOW = "presents_to_show";
+    public static int UNBOUGHT = 1;
+    public static int UNSENT = 2;
+
+
+    private List<GivenPresent> presentList = new ArrayList<>();
+    private CustomRecyclerView recyclerView;
+    private PresentsAdapter presentsAdapter;
+    private int presentsToShow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_presents);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Intent intent = getIntent();
+        presentsToShow = intent.getIntExtra(PRESENTS_TO_SHOW, UNBOUGHT);
+        getSupportActionBar().setTitle(presentsToShow == UNBOUGHT ? "Unbought presents" : "Unsent presents");
+
+        // load presents
+        DBHandler dbHandler = new DBHandler(this);
+        presentList = dbHandler.loadUnboughtPresents();
+
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setEmptyView(findViewById(R.id.empty));
+        presentsAdapter = new PresentsAdapter(presentList, this);
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this, R.drawable.divider);
+        recyclerView.addItemDecoration(dividerItemDecoration);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(presentsAdapter);
     }
 
+    @Override
+    public void onPresentClicked(View view, GivenPresent present) {}
 }
