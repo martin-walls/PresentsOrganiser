7381fa48055344bd00db91d4dabdc19d686d285b
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2018-11-21 21:21:35 +0000

    Database migration SQL to drop column PersonId in GivenPresents

--- app/src/main/assets/from_2_to_3.sql
+++ app/src/main/assets/from_2_to_3.sql
@@ -4,8 +4,35 @@ PersonId INTEGER,
 PRIMARY KEY (PresentId, PersonId),
 FOREIGN KEY (PresentId) REFERENCES GivenPresents (PresentId)
 ON DELETE CASCADE ON UPDATE NO ACTION,
-FOREIGN KEY (PersonId) REFERENCES `Names` (PersonId)
+FOREIGN KEY (PersonId) REFERENCES Names (PersonId)
 ON DELETE CASCADE ON UPDATE NO ACTION
 );
 
-ALTER TABLE GivenPresents
+INSERT INTO GivenPresents_Names (PresentId, PersonId)
+SELECT PresentId, PersonId
+FROM GivenPresents;
+
+PRAGMA foreign_keys=off;
+
+BEGIN TRANSACTION;
+
+ALTER TABLE GivenPresents RENAME TO _GivenPresents_old;
+
+CREATE TABLE GivenPresents (
+PresentId INTEGER PRIMARY KEY AUTOINCREMENT,
+Year INTEGER,
+Present TEXT NOT NULL,
+Notes TEXT,
+Bought INTEGER,
+Sent INTEGER
+)
+
+INSERT INTO GivenPresents (PresentId, Year, Present, Notes, Bought, Sent)
+SELECT PresentId, Year, Present, Notes, Bought, Sent
+FROM _GivenPresents_old;
+
+DROP TABLE _GivenPresents_old
+
+COMMIT;
+
+PRAGMA foreign_keys=on;
