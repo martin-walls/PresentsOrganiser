7ff2466ac1ad855b5543c6584aac2c140425b144
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2019-11-25 17:59:23 +0000

    migrate to AndroidX, update Gradle

--- app/src/main/java/com/martinwalls/presentsorganiser/database/DBHandler.java
+++ app/src/main/java/com/martinwalls/presentsorganiser/database/DBHandler.java
@@ -8,7 +8,7 @@ import android.database.sqlite.SQLiteConstraintException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Environment;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.text.TextUtils;
 import android.util.Log;
 
--- app/src/main/java/com/martinwalls/presentsorganiser/givenpresents/viewpresents/family/ViewMembersDialog.java
+++ app/src/main/java/com/martinwalls/presentsorganiser/givenpresents/viewpresents/family/ViewMembersDialog.java
@@ -5,10 +5,10 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import androidx.fragment.app.DialogFragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 
